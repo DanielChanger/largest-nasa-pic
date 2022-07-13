7af26f4fc8e1b9a3d4fab90d1a5c410648f98627
Danyl Miniailo <danyl.miniailo@care.com>
2022-07-13 20:33:41 +0300

    Use Caffeine cache provider

--- src/main/java/com/example/PictureConfig.java
+++ src/main/java/com/example/PictureConfig.java
@@ -2,18 +2,14 @@ package com.example;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
-@EnableCaching
-@EnableScheduling
 @Configuration
 public class PictureConfig {
 
